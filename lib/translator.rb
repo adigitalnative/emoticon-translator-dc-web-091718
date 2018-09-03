41b9ce7b713b565c0d915963d910e9c87f9f3cbb
Jacqueline Chenault <adigitalnative@gmail.com>
2018-09-03 18:03:18 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,24 @@
-# require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(file)
+  loaded_library = YAML.load_file(file)
+  
+  parsed_file = {
+    "get_meaning" => {},
+    "get_emoticon" => {}
+  }
+  
+  loaded_library.each do |key, values|
+    parsed_file["get_emoticon"][values[0]] = values[1]
+    parsed_file["get_meaning"][values[1]] = key
+  end
+  
+  parsed_file
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file, emoticon)
+  library = load_library(file)
 end
 
 def get_english_meaning
